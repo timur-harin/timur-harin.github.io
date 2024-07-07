2ef31ce82a247883736461f3a182e9c0fe3f70b1
Timur Harin <timurx404@gmail.com>
2024-07-07 17:52:24 +0300

    update fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -12,13 +13,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Timur Harin',
-        home: const SimpleWebPage(),
-        theme: ThemeData(
-          fontFamily: 'JetBrainsMono',
-          primarySwatch: Colors.green,
-        ));
+      debugShowCheckedModeBanner: false,
+      title: 'Timur Harin',
+      theme: ThemeData(
+        fontFamily: 'JetBrainsMono',
+        primarySwatch: Colors.orange,
+      ),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 900, name: TABLET),
+          const Breakpoint(start: 901, end: 1800, name: DESKTOP),
+          const Breakpoint(start: 1801, end: double.infinity, name: '4K'),
+        ],
+      ),
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          builder: (context) {
+            return MaxWidthBox(
+              maxWidth: 1800,
+              background: Container(color: Colors.transparent),
+              child: ResponsiveScaledBox(
+                width: ResponsiveValue<double>(
+                  context,
+                  defaultValue: 1800.0,
+                  conditionalValues: [
+                    const Condition.equals(name: MOBILE, value: 1800.0),
+                    const Condition.equals(name: TABLET, value: 1800.0),
+                    const Condition.equals(name: DESKTOP, value: 1800.0),
+                    const Condition.equals(name: '4K', value: 1800),
+                  ],
+                ).value,
+                child: const SimpleWebPage(),
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
 
@@ -42,6 +75,15 @@ class _SimpleWebPageState extends State<SimpleWebPage> {
   ];
 
   List<TextLaunch> listElements = [
+    TextLaunch(
+      url: "https://timur-harin.ru/assets/Flutter_resume_Timur_Harin.pdf",
+      text: "Резюме",
+    ),
+    TextLaunch(
+      url:
+          "https://github.com/timur-harin/Sum24FlutterCourse?tab=readme-ov-file#sum24fluttercourse",
+      text: "Провел курс по Flutter в Innopolis University",
+    ),
     TextLaunch(
         url: "https://t.me/code_v_meshke",
         text: "Телеграмм канал о кроссплатформенной разработке "),
@@ -69,89 +111,94 @@ class _SimpleWebPageState extends State<SimpleWebPage> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SingleChildScrollView(
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.all(MediaQuery.of(context).size.height / 20),
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius: MediaQuery.of(context).size.width / 20,
-                    backgroundImage: const AssetImage("assets/images/me.jpeg"),
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const CircleAvatar(
+                  radius: 150,
+                  backgroundImage: AssetImage("assets/images/me.jpeg"),
+                ),
+                const AutoSizeText(
+                  "Timur Harin",
+                  minFontSize: 40,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
-                  Text(
-                    "Timur Harin",
-                    style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width / 30,
-                      color: Colors.black,
-                    ),
+                ),
+                const AutoSizeText(
+                  "@timur_harin",
+                  minFontSize: 40,
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: Colors.black,
                   ),
-                  Text(
-                    "@timur_harin",
-                    style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width / 50,
-                      color: Colors.black,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (IconLaunch iconLaunch in topElements)
-                        InkWell(
-                          onTap: () async {
-                            await launchUrl(Uri.parse(iconLaunch.url));
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.all(10),
-                            width: MediaQuery.of(context).size.width / 30,
-                            height: MediaQuery.of(context).size.width / 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (IconLaunch iconLaunch in topElements)
+                      InkWell(
+                        onTap: () async {
+                          await launchUrl(Uri.parse(iconLaunch.url));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: SizedBox(
+                            width: 70,
+                            height: 70,
                             child: Image.asset(
                               iconLaunch.image,
                               color: Colors.black,
+                              fit: BoxFit.cover,
                             ),
                           ),
                         ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      for (TextLaunch textLaunch in listElements)
-                        InkWell(
-                          onTap: () async {
-                            await launchUrl(Uri.parse(textLaunch.url));
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                            width: MediaQuery.of(context).size.width / 2,
-                            margin: const EdgeInsets.all(10),
-                            child: Padding(
-                              padding: const EdgeInsets.all(15),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: AutoSizeText(
-                                      textLaunch.text,
-                                      style:
-                                          const TextStyle(color: Colors.white),
-                                      textAlign: TextAlign.center,
-                                      maxLines: 2,
-                                    ),
+                      ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    for (TextLaunch textLaunch in listElements)
+                      InkWell(
+                        onTap: () async {
+                          await launchUrl(Uri.parse(textLaunch.url));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.black.withOpacity(0.75),
+                          ),
+                          width: 1500,
+                          margin: const EdgeInsets.all(10),
+                          child: Padding(
+                            padding: const EdgeInsets.all(15),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: AutoSizeText(
+                                    textLaunch.text,
+                                    minFontSize: 25,
+                                    style: const TextStyle(color: Colors.white),
+                                    textAlign: TextAlign.center,
+                                    maxLines: 2,
                                   ),
-                                  const Icon(
-                                    Icons.arrow_outward,
-                                    color: Colors.white,
-                                  )
-                                ],
-                              ),
+                                ),
+                                const Icon(
+                                  Icons.arrow_outward,
+                                  color: Colors.white,
+                                )
+                              ],
                             ),
                           ),
                         ),
-                    ],
-                  )
-                ],
-              ),
+                      ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
